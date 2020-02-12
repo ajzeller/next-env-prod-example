ccebec9908dc51f19161f85cf2ca377de9b46bc8
Andrew Zeller <andrew.j.zeller@gmail.com>
2020-02-11 16:27:59 -0800

    initial commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+module.exports = {
+  env: {
+    DEMO_KEY: process.env.DEMO_KEY
+  },
+}
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+export default function Index(){
+  return(
+    <h1>{process.env.DEMO_KEY}</h1>
+  )
+}
+  
